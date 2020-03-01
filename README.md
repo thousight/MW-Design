8ab20596da272e2e8ce906b3ed9f8ed59ea92b9d
Mark Wen <markwenguojie94@gmail.com>
2020-02-29 20:05:48 -0800

    chore: added npm badge to readme, remove redundant test, fixed huskyrc

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+[![npm version](https://badge.fury.io/js/mw-design.svg)](https://badge.fury.io/js/mw-design)
 [![Build Status](https://travis-ci.com/thousight/MW-Design.svg?branch=master)](https://travis-ci.com/thousight/MW-Design)
 [![Coverage Status](https://coveralls.io/repos/github/thousight/MW-Design/badge.svg?branch=master)](https://coveralls.io/github/thousight/MW-Design?branch=master)
 [![Greenkeeper badge](https://badges.greenkeeper.io/thousight/MW-Design.svg)](https://greenkeeper.io/)
--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,17 +1,11 @@
-import React from 'react';
-import { render } from 'enzyme';
+import React from 'react'
+import { render } from 'enzyme'
 
-import Button from './';
+import Button from './'
 import theme from '../../themes/theme'
 
 it('should be rendered', () => {
-	const rendered = render(<Button theme={theme}>Button</Button>);
+  const rendered = render(<Button theme={theme}>Button</Button>)
 
-	expect(rendered).toBeDefined();
-});
-
-it('should be rendered without borderRadius', () => {
-	const rendered = render(<Button theme={{ ...theme, borderRadius: undefined }}>Button</Button>);
-
-	expect(rendered).toBeDefined();
-});
+  expect(rendered).toBeDefined()
+})
